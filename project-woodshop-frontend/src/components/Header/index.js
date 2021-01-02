a8e30184cb85ff9bf387e90e8e56229fd510f6bb
Ethan Torres <torresethan99@gmail.com>
2021-01-01 21:43:33 -0600

    proceeded to add code, will continue tommorow

---
+++ project-woodshop-frontend/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import{ Navigation } from '..';
+
+function Header() {
+    return(
+        <header className="Header">
+            <h1>Project Woodshoppe</h1>
+            <Navigation></Navigation>
+        </header>
+    )
+}
