fd6e2e7c13210032f60adec84775b32d1d3ef990
Ethan Torres <torresethan99@gmail.com>
2021-02-20 21:17:07 -0600

    created layouts for server and frontend

---
+++ project-woodshop-frontend/src/components/Homepage/index.js
@@ -0,0 +1,11 @@
+
+function Homepage(props){
+    return (
+        <div className="navigation">
+            <p>insert navigation here</p>
+        </div>
+    );
+}
+
+
+export default Homepage;
---
+++ project-woodshop-frontend/src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useParams,
+} from "react-router-dom";
+
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+
+
+function Navigation(props){
+    return (
+        <div className="navigation">
+            <p>insert navigation here</p>
+        </div>
+    );
+} 
+
+
+
+
+export default Navigation;
